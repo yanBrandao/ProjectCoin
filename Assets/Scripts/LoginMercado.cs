3d6581ec79c8068b9f8fcba50d1f14b20e1f77b0
Yan Diniz <yan.diniz@itbam.org.br>
2018-11-13 11:42:42 -0400

    [WorkFlow] Adicionado cenas para seguir o fluxo de telas da aplicação

---
+++ Assets/Scripts/LoginMercado.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+public class LoginMercado : MonoBehaviour {
+
+    bool btnPressed;
+    public Text Login;
+    public Text Senha;
+
+    static string PasswordHash = string.Empty;
+    static readonly string SaltKey = "Key@App";
+    static readonly string VIKey = "@BitCoin2018";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (btnPressed)
+            Debug.Log("Do something now");
+    }
+
+    public void OnMouseClick()
+    {
+
+        PasswordHash = Senha.text;
+    }
+
+    public static string Encrypt(string plainText)
+    {
+        byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+
+        byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+        var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.Zeros };
+        var encryptor = symmetricKey.CreateEncryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+
+        byte[] cipherTextBytes;
+
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+            {
+                cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+                cryptoStream.FlushFinalBlock();
+                cipherTextBytes = memoryStream.ToArray();
+                cryptoStream.Close();
+            }
+            memoryStream.Close();
+        }
+        return Convert.ToBase64String(cipherTextBytes);
+    }
+    public static string Decrypt(string encryptedText)
+    {
+        byte[] cipherTextBytes = Convert.FromBase64String(encryptedText);
+        byte[] keyBytes = new Rfc2898DeriveBytes(PasswordHash, Encoding.ASCII.GetBytes(SaltKey)).GetBytes(256 / 8);
+        var symmetricKey = new RijndaelManaged() { Mode = CipherMode.CBC, Padding = PaddingMode.None };
+
+        var decryptor = symmetricKey.CreateDecryptor(keyBytes, Encoding.ASCII.GetBytes(VIKey));
+        var memoryStream = new MemoryStream(cipherTextBytes);
+        var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+        byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+
+        int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+        memoryStream.Close();
+        cryptoStream.Close();
+        return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount).TrimEnd("\0".ToCharArray());
+    }
+}
---
+++ Assets/Scripts/MercadoAnimation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class MercadoAnimation : MonoBehaviour, IPointerClickHandler
+{
+
+    private Animator animController;
+    public bool isUp;
+
+    // Use this for initialization
+    void Start () {
+        animController = GetComponent<Animator>();
+        isUp = animController.GetBool("isUp");
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveButton()
+    {
+        isUp = animController.GetBool("isUp");
+        animController.SetBool("isUp", !isUp);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ClickEventTriggered();
+    }
+
+    /*Click Event*/
+    [Serializable]
+    public class ClickEvent : UnityEvent { }
+
+    [SerializeField]
+    private ClickEvent myOwnEvent = new ClickEvent();
+    public ClickEvent onMyOwnEvent { get { return myOwnEvent; } set { myOwnEvent = value; } }
+
+    public void ClickEventTriggered()
+    {
+        MoveButton();
+        int childrens = transform.childCount;
+        for(int i = 0; i < childrens; i++)
+        {
+            Transform go = transform.GetChild(i);
+            go.gameObject.SetActive(!isUp);
+        }
+
+    }
+}
